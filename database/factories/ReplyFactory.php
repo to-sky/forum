6c4c4d7d968e9ac17e809a1492f659a273797b79
Dmitry Tolsky <dmitry.tolsky@rhinoda.com>
2018-01-18 15:50:57 +0600

    crated Thread, Reply migrations and factories

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function() {
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function() {
+            return factory('App\Thread')->create()->id;
+        },
+        'body' => $faker->paragraph
+    ];
+});
