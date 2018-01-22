2da6e4bf050a6b1c3616bb0aedeb5c57a12cbd7d
tosky <dima.tolsky@gmail.com>
2018-01-22 18:09:56 +0600

    added validation to reply and thread with tests

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -12,8 +12,7 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function unauthenticated_user_may_not_add_replies()
     {
-        $this->withExceptionHandling()
-            ->post('/threads/some-channel/1/replies')
+        $this->post('/threads/some-channel/1/replies')
             ->assertRedirect('/login');
     }
 
@@ -29,5 +28,17 @@ class ParticipateInForumTest extends TestCase
 
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
