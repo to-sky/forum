d8823ebe353f234ac937be7fdf3beeefaaed22a2
tosky <dima.tolsky@gmail.com>
2018-01-22 13:51:03 +0600

    added CreateThreadsTest

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -10,6 +10,17 @@ class ReplyTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function unauthenticated_user_cant_create_reply()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->create();
+
+        $this->post($thread->path().'/replies', $reply->toArray())
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
     public function it_has_an_owner()
     {
         $reply = factory('App\Reply')->create();
