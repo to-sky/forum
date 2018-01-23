473fad7ad9d99b100598bf847e0f27308ddf0cc1
tosky <dima.tolsky@gmail.com>
2018-01-23 19:14:40 +0600

    added replies paginate

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,15 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     /**
      * Get a string path for the thread.
      *
